431d337e41b4901a761d08d6722797dfec540ff7
thisismattsmith <hello@thisismattsmith.com>
2021-12-08 13:33:31 +1100

    Style updates - metadata and Home page heading

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ const Layout = ({ location, title, children }) => {
   if (isRootPath) {
     header = (      
       <h1 className="main-heading">
-        <Link to="/">{title}</Link>
+        {title}
       </h1>
     )
   } else {
