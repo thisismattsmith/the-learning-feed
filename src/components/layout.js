95406a1cbe8cef7ec83bda171f9d13e68a94c1b5
thisismattsmith <hello@thisismattsmith.com>
2022-04-17 17:15:42 +1000

    layout change
    change blog post (feed item) top heading from title to return to home

--- src/components/layout.js
+++ src/components/layout.js
@@ -17,7 +17,7 @@ const Layout = ({ location, title, children }) => {
   } else {
     header = (
       <Link className="header-link-home" to="/">
-        {title}
+        ↵ Return to the feed
       </Link>
     )
   }
